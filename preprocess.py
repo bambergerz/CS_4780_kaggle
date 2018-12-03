004a12f3db114b896ecee0e8f5fc0187bb5f2a47
Zachary Bamberger <zeb3@cornell.edu>
2018-12-03 18:51:25 -0500

    created word embeddings to treat sentence portion of data. Will then do cosine similarity or similar metric between average Trump tweets and this particular sentence

--- preprocess.py
+++ preprocess.py
@@ -1,15 +1,33 @@
+# Libraries
 import os
 import numpy as np
 import pandas as pd
-
+import nltk
+import gensim
+from nltk.corpus import brown
 from sklearn.model_selection import train_test_split
-from sklearn import svm
+
+# Local imports
+import svm
+
+
+# General pre-processing methods
+def get_embeddings():
+    if "brown.embedding" in os.listdir(os.getcwd()):
+        return gensim.models.Word2Vec.load('brown.embedding')
+    nltk.download("brown")
+    model = gensim.models.Word2Vec(brown.sents())
+    model.save("brown.embedding")
+    return model
+
 
 if __name__ == "__main__":
 
     xDF = pd.read_csv(filepath_or_buffer="train.csv")
     xDF = xDF.drop("id", 1)
     X = xDF.values
+
+    print("Titles: " + str(xDF.columns.values))
 
     yDF = xDF.pop("label")
     Y = yDF.values
@@ -19,14 +37,5 @@ if __name__ == "__main__":
 
     ### SVM ###
 
-    kernel_types = ["linear", "poly", "rbf", "sigmoid"]
-    classifiers = []
-    for k_type in kernel_types:
-        # enable probability to True for bootstrapping
-        c = svm.SVC(kernel=k_type, probability=True)
-
-        # TODO: normalize data so that everything is a float.
-        # That way, can fit SVM model
-
-        # c.fit(xTr, yTr)
-        # classifiers.append(c)
+    word_embeddings = get_embeddings()
+    svm.svm_eval(xTr, yTr)
--- svm.py
+++ svm.py
@@ -1,2 +1,24 @@
 from sklearn import svm
+import gensim
+import nltk
+from nltk.corpus import brown
+import os
+
+
+### SVM ###
+
+def svm_eval(xTr, yTr):
+    kernel_types = ["linear", "poly", "rbf", "sigmoid"]
+    classifiers = []
+    for k_type in kernel_types:
+        # enable probability to True for bootstrapping
+        c = svm.SVC(kernel=k_type, probability=True)
+
+        # TODO: normalize data so that everything is a float.
+        # That way, can fit SVM model
+
+        c.fit(xTr, yTr)
+        classifiers.append(c)
+
+
 
