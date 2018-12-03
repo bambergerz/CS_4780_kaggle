c9ffe91248cbd9ec464d5803a721e7cd6ad923b5
Zachary Bamberger <zeb3@cornell.edu>
2018-12-03 00:20:26 -0500

    simplified implementation of pre-process such that it now relies on Pandas

--- preprocess.py
+++ preprocess.py
@@ -1,32 +1,48 @@
 import os
 import numpy as np
+import pandas as pd
 
+from sklearn.model_selection import train_test_split
 
 if __name__ == "__main__":
-    with open("train.csv", "r") as fileHandle:
-        lines = fileHandle.readlines()
 
-    titles = lines.pop(0).split(",")
-    index = 1
-    samples = {}
+    xDF = pd.read_csv(filepath_or_buffer="train.csv")
+    xDF = xDF.drop("id", 1)
+    X = xDF.values
 
-    for line in lines:
-        element_dict = {}
-        _, line_list = line.split(",", 1)
-        line_list = line_list.split(",")
-        for i in range(len(titles) - 1):
-            arg = line_list[i]
-            element_dict[titles[i]] = arg
-        samples[index] = element_dict
-        index += 1
+    yDF = xDF.pop("label")
+    Y = yDF.values
 
-    # splitting data: 80% training, 20% val
     # TODO: cross validation here
-    val_size = int(0.2 * index)
-    val_keys = np.random.choice(list(samples.keys()), val_size, replace=False)
+    xTr, xVer, yTr, yVer = train_test_split(X, Y, test_size=0.8)
 
-    val_set = {}
-    for k in val_keys:
-        val_set[k] = samples.pop(k)
+    # Please disregard code below:
 
-    print(val_set)
+    # with open("train.csv", "r") as fileHandle:
+    #     lines = fileHandle.readlines()
+    #
+    # titles = lines.pop(0).split(",")
+    # index = 1
+    # samples = {}
+    #
+    # for line in lines:
+    #     element_dict = {}
+    #     _, line_list = line.split(",", 1)
+    #     line_list = line_list.split(",")
+    #     for i in range(len(titles) - 1):
+    #         arg = line_list[i]
+    #         element_dict[titles[i]] = arg
+    #     samples[index] = element_dict
+    #     index += 1
+    #
+    # # splitting data: 80% training, 20% val
+    # # TODO: cross validation here
+    # val_size = int(0.2 * index)
+    # val_keys = np.random.choice(list(samples.keys()), val_size, replace=False)
+    #
+    # val_set = {}
+    # for k in val_keys:
+    #     val_set[k] = samples.pop(k)
+    #
+    # xTr = np.zeros((len(titles), len(samples.keys())))
+    # xVal = np.zeros((len(titles), len(val_keys)))
