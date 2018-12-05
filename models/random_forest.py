80d41291a2078228425fa2b5dfd0c4216b08f825
Zachary Bamberger <zeb3@cornell.edu>
2018-12-05 17:08:42 -0500

    added bootstrapped/non-bootstrapped models to data folder in repo.
    implemented evaluation of random forest models

--- models/random_forest.py
+++ models/random_forest.py
@@ -1,6 +1,7 @@
 from sklearn.ensemble import RandomForestClassifier
 import pickle
 import os
+import numpy as np
 
 #####################
 ### Random Forest ###
@@ -8,17 +9,44 @@ import os
 
 
 def generate_rf_classifiers(xTr, yTr):
-    os.chdir("data")
-    n = 10
-    print("Generating classifier")
-    clf = RandomForestClassifier(n_estimators=n)
-    print("Fitting classifier")
-    clf.fit(xTr, yTr)
-    print("Done fitting random forest model. Saving model")
-    s = pickle.dumps(clf)
-    with open("rf_" + str(n) + ".pickle", 'wb') as fileHandle:
-        print("Writing rf_" + str(n) + ".pickle to " + os.getcwd())
-        fileHandle.write(s)
-    os.chdir("..")
-    print("All done!!!")
-    return clf
+    cwd = os.getcwd()
+    os.chdir(os.path.join("data", "random_forest_models"))
+    poss_ns = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200,
+               300, 400, 500, 1000]
+    classifiers = []
+    for n in poss_ns:
+
+        clf = RandomForestClassifier(n_estimators=n, bootstrap=False)
+        clf_bootstrap = RandomForestClassifier(n_estimators=n, bootstrap=True)
+        clf.fit(xTr, yTr)
+        clf_bootstrap.fit(xTr, yTr)
+        classifiers.append(clf)
+        classifiers.append(clf_bootstrap)
+        s_1 = pickle.dumps(clf)
+        s_2 = pickle.dumps(clf_bootstrap)
+        with open("rf_" + str(n) + ".pickle", 'wb') as fileHandle:
+            fileHandle.write(s_1)
+        with open("rf_" + str(n) + "_bootstrap.pickle", "wb") as fileHandle:
+            fileHandle.write(s_2)
+    os.chdir(cwd)
+    return classifiers
+
+
+def evaluate_classifiers(models, xVer, yVer):
+    """
+
+    :param models: a list of models to compare
+    :param xVer: the input data of the verification set
+    :param yVer: the tags of the verification set
+    :return:
+    """
+    outputs = []
+    for model in models:
+        output = model.predict(xVer)
+        similar = np.sum(output, yVer)
+        total = yVer.shape[0]
+        accuracy = similar / total
+        outputs.append((output, accuracy))
+
+
+
