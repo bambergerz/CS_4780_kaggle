59bbcaf60e557f5b4b2916281c05b10c1f62de1c
anyachopra97 <anyachopra97@gmail.com>
2018-12-04 21:43:38 -0500

    Added Random Forest

---
+++ random_forest.py
@@ -0,0 +1,16 @@
+from sklearn import svm
+import gensim
+import nltk
+from nltk.corpus import brown
+import os
+import pickle
+
+
+### Random Forest ###
+
+def generate_svm_classifiers(xTr, yTr):
+    clf=RandomForestClassifier(n_estimators=100)
+    clf.fit(X_train,y_train)
+    pickle.dump(clf, "randomForest.model")
+    print("All done!!!")
+    return clf
